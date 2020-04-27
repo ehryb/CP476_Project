4832852234f69f00487ff172fe2c20a25dd643fb
koro7020 <koro7020@mylaurier.ca>
2020-04-27 09:26:58 -0400

    myaccountpage works properly now

--- myaccountpage.php
+++ myaccountpage.php
@@ -16,6 +16,10 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main Page</title>
     <link href="project_css.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script type="text/javascript" src="JS/errorChecking.js"></script>
 </head>
@@ -61,7 +65,7 @@ session_start();
         <h3> My Account </h3>
         <img id = "accountImage" src = "Images/AccountImage.jpg" alt="Logo">
 
-        <form action="updateDB.php" method="POST" id ="change-this" action = "#">
+        <form action="updateDB.php" method="POST" id ="change-this">
             <h4>User Name: </h4>
 
             <?php echo $username ?>
