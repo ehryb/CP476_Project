0ffb94a6d5614bddcc7ec9adb637db0bb9c37362
koro7020 <koro7020@mylaurier.ca>
2020-04-27 16:37:29 -0400

    Added Microsoft URL

--- include/config.php
+++ include/config.php
@@ -1,20 +1,8 @@
 <?php
 
-
-/*
-define('DBHOST', 'localhost'); #127.0.0.1
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBNAME', 'projectdb');
-
-*/
-
 define('DBHOST', 'reviewsitejayem-mysqldbserver.mysql.database.azure.com'); #127.0.0.1
 define('DBUSER', 'mysqlemjay@reviewsitejayem-mysqldbserver');
 define('DBPASS', 'JayEm2345');
 define('DBNAME', 'projectdb');
-
-
-
 
 ?>
